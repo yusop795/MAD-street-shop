1d3eda22343171311fb02e995689927ee568f38f
yusop <yusop795@gmail.com>
2020-03-29 12:01:43 +0900

    Home 페이지에 모달 띄우기 - 서치모달

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -5,28 +5,34 @@ import { HomeHeader } from '../components/Header'
 import { MainMap } from '../components/Map';
 import { ShopInfo } from '../components/Unit';
 
+import ModalPageUtill from '../util/ModalPageUtill.js';
+
+import { SearchModal } from '../containers/ModalPage';
+
 const Home = () => {
   const [location, setLocation] = useState('');
-  const [shopList, setShopList] = useState([{name:'토토네 튀김', latitude:37.489524599999996,longitude:126.98655099999998},{name:'네네치킨', latitude:37.489524599999996,longitude:126.98643099999998}]);
+  const [shopList, setShopList] = useState([{ name: '토토네 튀김', latitude: 37.489524599999996, longitude: 126.98655099999998 }, { name: '네네치킨', latitude: 37.489524599999996, longitude: 126.98643099999998 }]);
   const loading = useSelector(({ authReducer }) => authReducer.loading, true);
 
-    // 위치정보 조회
-    const fetchGeolocation = () => {
-      const options = {
-        enableHighAccuracy: true,
-        maximumAge: 300000,
-        timeout: 50000,
-      };
-  
-      navigator.geolocation.getCurrentPosition(
-        ({ coords }) => {
-          console.log('coords', coords);
-          setLocation({ lat: coords.latitude, long: coords.longitude });
-        },
-        e => console.log(`Geolocation 오류 [${e.code}] : ${e.message}`),
-        options,
-      );
+  // 위치정보 조회
+  const fetchGeolocation = () => {
+    const options = {
+      enableHighAccuracy: true,
+      maximumAge: 300000,
+      timeout: 50000,
     };
+
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        console.log('coords', coords);
+        setLocation({ lat: coords.latitude, long: coords.longitude });
+      },
+      e => console.log(`Geolocation 오류 [${e.code}] : ${e.message}`),
+      options,
+    );
+  };
+
+  const { targetModalPage, isModalOpen, setModalPage } = ModalPageUtill();
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -44,27 +50,37 @@ const Home = () => {
     }
   }, [location]);
 
+  const rederModalPage = () => {
+    switch (targetModalPage) {
+      case 'SearchModal':
+        return <SearchModal isOpen={isModalOpen} onEvent={setModalPage} />;
+      default:
+        return null;
+    }
+  }
   return (
-    <div>
-      <HomeHeader fetchGeolocation={fetchGeolocation}/>
-      <MainMap location={location} setLocation={setLocation} shopList={shopList}/>
-      <ShopInfo 
+    // <div>{process.env.REACT_APP_API_KEY}</div>
+    <div style={{ background: 'gray' }}>
+      <HomeHeader fetchGeolocation={fetchGeolocation} setModalPage={setModalPage} />
+      <MainMap location={location} setLocation={setLocation} shopList={shopList} />
+      <ShopInfo
         shopInfo={{
-            name: "석정포장마차",
-            category: "분식",
-            photo: "https://post-phinf.pstatic.net/MjAxOTEwMDFfNjkg/MDAxNTY5OTE5NzUxNDc2.mnGT1DcIaEY9os4ftETl5Bc_SudAwsUq8O3KaqlpQtQg.qhcMdUjcKqBoTC6hR1j7OnsY4BIpK1aulSmv0mlwO14g.JPEG/%EB%B6%84%EC%8B%9D.jpg?type=w1200",
-            open: true,
-            link: "/",
-            info: {
-              distance: 8,
-              like: 674,
-              userLike: true,
-            }}
+          name: "석정포장마차",
+          category: "분식",
+          photo: "https://post-phinf.pstatic.net/MjAxOTEwMDFfNjkg/MDAxNTY5OTE5NzUxNDc2.mnGT1DcIaEY9os4ftETl5Bc_SudAwsUq8O3KaqlpQtQg.qhcMdUjcKqBoTC6hR1j7OnsY4BIpK1aulSmv0mlwO14g.JPEG/%EB%B6%84%EC%8B%9D.jpg?type=w1200",
+          open: true,
+          link: "/",
+          info: {
+            distance: 8,
+            like: 674,
+            userLike: true,
+          }
+        }
         }
         fetchGeolocation={fetchGeolocation}
       />
-    </div>
+      {rederModalPage()}
+    </div >
   );
-};
-
+}
 export default withRouter(Home);
--- src/containers/ModalPage/index.js
+++ src/containers/ModalPage/index.js
@@ -1,2 +1,3 @@
 export { default as SettingCategory } from './SettingCategory';
 export { default as SettingTime } from './SettingTime';
+export { default as SearchModal } from './SearchModal';
