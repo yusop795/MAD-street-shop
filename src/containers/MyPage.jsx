8b78821d2102a2267024c3b88f52240bf0a4c578
kimhaein <rlagpdls93@gmail.com>
2020-04-06 20:07:09 +0900

    마이페이지에 비회원 조건 추가

--- src/containers/MyPage.jsx
+++ src/containers/MyPage.jsx
@@ -8,6 +8,7 @@ import { Alert } from '../components/Alert';
 
 import imgProfile01 from '../assets/imgs/imgProfile01.png';
 import imgProfile02 from '../assets/imgs/imgProfile02.png';
+import imgProfile03 from '../assets/imgs/imgProfile03.png';
 import iconDeclare from '../assets/imgs/iconDeclare.png';
 import iconHeart from '../assets/imgs/iconHeart.png';
 import iconSettings from '../assets/imgs/iconSettings.png';
@@ -18,39 +19,50 @@ import '../assets/styles/containers/myPage.scss';
 
 const MyPage = ({ history, match }) => {
   const type = 'user';
+  const isLogin = true;
 
-    // alert
-    const { isShowing, title, contents, setAlert} = AlertUtil();
+  // alert
+  const { isShowing, title, contents, setAlert} = AlertUtil();
 
   return (
     <div className="main myPage">
       <ModalHeader onEvent={history.goBack} border={false}/>
-      <div className="userInfoBox">
-        <img src={type ==='user'?imgProfile01:imgProfile02} className="userImg" alt="기본 프로필 이미지"/>
-        <div className="userInfo">
-          {type ==='user'? <span>일반회원</span> : <span>사장님</span>}
-          <p>김키모</p>
+      {!isLogin ? 
+      (
+        <div className="userInfoBox">
+          <img src={imgProfile03} className="userImg" alt="기본 프로필 이미지"/>
+          <p><Link to="/ranking">로그인</Link>이 필요한 서비스입니다</p>
         </div>
-      </div>
-      <div className="menuList">
-        <div className="menuItem">
-          <img src={iconSettings} alt="취향설정"/>
-          <p>취향설정</p>
-        </div>
-        <div className="menuItem">
-          <img src={iconHeart} alt="관심리스트"/>
-          <p>관심리스트</p>
-        </div>
-        <div className="menuItem" 
-          onClick={()=>{
-          setAlert({
-            contents:'준비 중인 기능입니다.'
-          })
-        }}>
-          <img src={iconDeclare} alt="신고리스트"/>
-          <p>신고리스트</p>
-        </div>
-      </div>
+      ):(
+        <>
+          <div className="userInfoBox">
+            <img src={type ==='user'?imgProfile01:imgProfile02} className="userImg" alt="기본 프로필 이미지"/>
+            <div className="userInfo">
+              {type ==='user'? <span>일반회원</span> : <span>사장님</span>}
+              <p>김키모</p>
+            </div>
+          </div>
+          <div className="menuList">
+            <div className="menuItem">
+              <img src={iconSettings} alt="취향설정"/>
+              <p>취향설정</p>
+            </div>
+            <div className="menuItem">
+              <img src={iconHeart} alt="관심리스트"/>
+              <p>관심리스트</p>
+            </div>
+            <div className="menuItem" 
+              onClick={()=>{
+              setAlert({
+                contents:'준비 중인 기능입니다.'
+              })
+            }}>
+              <img src={iconDeclare} alt="신고리스트"/>
+              <p>신고리스트</p>
+            </div>
+          </div>
+        </>
+      )}
       <div className="banner">
         <p>
           <span>스트릿푸드를 판매하고 계신가요?</span>
@@ -63,7 +75,7 @@ const MyPage = ({ history, match }) => {
         <li className="settingItem border">FAQ<img src={iconChevronRight} alt="이동"/></li>
         <li className="settingItem">로그아웃<img src={iconChevronRight} alt="이동"/></li>
       </ul>
-      <p className="notice">매드스트릿샵을 탈퇴하려면 <span>여기</span>를 눌러주세요.</p>
+      <p className="notice">매드스트릿샵을 탈퇴하려면 <b>여기</b>를 눌러주세요.</p>
       <Alert isShowing={isShowing} hide={setAlert} title={title} contents={contents}/>
     </div>
   );
